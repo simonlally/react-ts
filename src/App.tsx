cb767a1fd90f5d9133b04840dc3e39abee0a668e
simon lally <simonlallydev@gmail.com>
2020-03-31 22:53:50 -0400

    Initial repository

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        yay typescript
+      </div>
+    </div>
+  );
+}
+
+export default App;
